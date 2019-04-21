77d6eca8ed18f609b7e31b26c59abf9a537f855e
dongheon1997 <48716569+dongheon1997@users.noreply.github.com>
2019-04-21 12:54:52 +0900

    Update first

---
+++ Player.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class Player
+{
+private:
+	int hp[4];
+	int mp[4];
+	int pa[3], ma[3];
+	int pd[3], md[3];
+	int speed[3];
+	int state;
+	int bag[30];
+	int gold;
+	int equipment[5];
+
+public:
+
+};
+
+class Monster
+{
+private:
+	int hp[2];
+	int mp[2];
+	int pa, ma;
+	int pd, md;
+	int speed;
+	int state;
+	int bag[30];
+	int gold;
+
+public:
+
+};
